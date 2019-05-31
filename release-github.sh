e2c27938f1a968185f5cef93812ea7ad66b3b3e9
levy <897895407@qq.com>
2019-05-31 16:31:59 +0800

    fullfil github repos

--- release-github.sh
+++ release-github.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-repos=(data-list el-select-area)
+repos=(data-list el-select-area upload-to-ali el-data-table el-semver-input img-preview v-editor el-data-tree vue-sfc-cli el-form-renderer excel-it el-number-range nuxt-element-dashboard)
 
 for i in ${repos[@]}
 do
