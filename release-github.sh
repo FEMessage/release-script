e86f637757a94abad64a96d5a16d28daf46a4b0d
levy <897895407@qq.com>
2019-05-31 17:43:04 +0800

    set -e

--- release-github.sh
+++ release-github.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 repos=(data-list el-select-area upload-to-ali el-data-table el-semver-input img-preview v-editor el-data-tree vue-sfc-cli el-form-renderer excel-it el-number-range nuxt-element-dashboard)
 
 for i in ${repos[@]}
