333ce94bb48c150f63fb56f1a51bed0fe4526ae5
levy <897895407@qq.com>
2019-06-03 13:38:27 +0800

    fix: title style

--- get-release-log.sh
+++ get-release-log.sh
@@ -22,7 +22,7 @@ do
   html_url=`cat $resp_tmp_file | sed -n 5p | sed 's/\"html_url\"://g' | awk -F '"' '{print $2}'`
   body=`cat $resp_tmp_file | grep body | sed 's/\"body\"://g;s/\"//g'`
 	title=`echo $html_url | cut -d '/' -f 5`
-	echo "#[$title]($html_url)" >> $release_file
+	echo "# [$title]($html_url)" >> $release_file
 	echo $body >> $release_file
 done
 
