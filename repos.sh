4d124095005d8fdb8c12de4b776f0b3ceaf9e1e5
levy <897895407@qq.com>
2019-06-03 13:52:24 +0800

    fix: repos error

--- repos.sh
+++ repos.sh
@@ -1,1 +1,1 @@
-repos=(data-list el-select-area upload-to-ali el-data-table el-semver-input img-preview v-editor el-data-tree vue-sfc-cli el-f    orm-renderer excel-it el-number-range nuxt-element-dashboard)
+repos=(data-list el-select-area upload-to-ali el-data-table el-semver-input img-preview v-editor el-data-tree vue-sfc-cli el-form-renderer excel-it el-number-range nuxt-element-dashboard)
