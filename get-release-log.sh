7a81efbc2cc1587c2534363ac9a9b87c8462eba4
levy <897895407@qq.com>
2019-11-04 10:59:16 +0800

    feat: 显示版本号

--- get-release-log.sh
+++ get-release-log.sh
@@ -14,11 +14,14 @@ for i in ${repos[@]}
 do
   url="https://api.github.com/repos/FEMessage/$i/releases/latest"
   curl $url > $resp_tmp_file
-  html_url=`cat $resp_tmp_file | sed -n 5p | sed 's/\"html_url\"://g' | awk -F '"' '{print $2}'`
-  body=`cat $resp_tmp_file | grep body | sed 's/\"body\"://g;s/\"//g'`
-	title=`echo $html_url | cut -d '/' -f 5`
-	echo "## [$title]($html_url)" >> $release_file
-	echo $body >> $release_file
+
+  html_url=$(sed -n 5p $resp_tmp_file | sed 's/\"html_url\"://g' | awk -F '"' '{print $2}')
+  body=$(grep body < $resp_tmp_file | sed 's/\"body\"://g;s/\"//g')
+  title=$(echo $html_url | cut -d '/' -f 5)
+  version=$(echo $html_url | awk -F '/' '{print $NF}')
+
+  echo "## [$title $version]($html_url)" >> $release_file
+  echo $body >> $release_file
 done
 
 rm $resp_tmp_file
