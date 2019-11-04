e26b07b282e3515dfb01b6bb88d8a1798c18a714
levy <897895407@qq.com>
2019-11-04 10:50:31 +0800

    feat: 添加v-img/vant

--- repos.sh
+++ repos.sh
@@ -1,6 +1,6 @@
 repos=$@
 if [ $# -eq 0 ]
 then
-  repos=(data-list el-select-area upload-to-ali el-data-table el-semver-input img-preview v-editor el-data-tree vue-sfc-cli el-form-renderer excel-it el-number-range create-nuxt-app)
+  repos=(data-list el-select-area upload-to-ali el-data-table el-semver-input img-preview v-editor el-data-tree vue-sfc-cli el-form-renderer excel-it el-number-range create-nuxt-app v-img vant)
 fi
 echo ${repos[@]}
