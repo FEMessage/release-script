47b46689dd13a9824de1a9c2e85742f313b8da29
levy <897895407@qq.com>
2019-11-04 10:52:34 +0800

    chore: 删除过时代码

--- get-release-log.sh
+++ get-release-log.sh
@@ -13,11 +13,6 @@ echo "# $date" > $release_file
 for i in ${repos[@]}
 do
   url="https://api.github.com/repos/FEMessage/$i/releases/latest"
-  if [ "$i" == "nuxt-element-dashboard" ]
-  then
-    # pc端框架仓库地址在另一个地方
-    url="https://api.github.com/repos/levy9527/nuxt-element-dashboard/releases/latest"
-  fi
   curl $url > $resp_tmp_file
   html_url=`cat $resp_tmp_file | sed -n 5p | sed 's/\"html_url\"://g' | awk -F '"' '{print $2}'`
   body=`cat $resp_tmp_file | grep body | sed 's/\"body\"://g;s/\"//g'`
