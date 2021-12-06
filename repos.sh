219a7ec3e8024b74c124fbcf9015bc9ec9481c3d
levy <levy9527@gmail.com>
2021-12-06 15:15:45 +0800

    Update repos.sh

--- repos.sh
+++ repos.sh
@@ -1,6 +1,7 @@
 repos=$@
 if [ $# -eq 0 ]
 then
-  repos=(data-list el-select-area upload-to-ali el-data-table el-semver-input img-preview v-editor el-data-tree vue-sfc-cli el-form-renderer excel-it el-number-range create-nuxt-app v-img vant)
+  echo "please input project name"
+  exit 1
 fi
 echo fetching releases: ${repos[@]}
